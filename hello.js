b30471779642d63c5dd90d1cf1bde63104b23514
sil3nt884 <ricky.luu@and.digital>
2023-01-28 10:39:23 +0000

    user validation

--- hello.js
+++ hello.js
@@ -50,6 +50,12 @@ const userInput= async () => {
  *
  *
  */
+
+function checkOptions (selectedUserOption, options) {
+    return (selectedUserOption in options);
+
+
+}
 function saveUserOption(selectedOption, userPizza) {
     userPizza.push(selectedOption)
 }
@@ -60,46 +66,85 @@ function saveUserOption(selectedOption, userPizza) {
         1: { bread : { thin : "thin", thick : "thick"}},
         2: {crust : { garlic : "garlic sprinkled crust", stuffed : "stuffed crust"}},
         3: {sauce : { tomato : "tomato", bbq : "bbq", none : "no sauce"}},
-        4: {cheese : { moz : "mozzarella", cheddar : "cheddar", vegan : "vegan cheese", noCheese : "no cheese"}},
-        5: {toppings : { pep : "pepperoni", veggie : "vegetables", meat : "spicy beef", tropical : "ham and pineapple"}},
+        4: {cheese : { mozzarella : "mozzarella", cheddar : "cheddar", vegan : "vegan cheese", "no cheese" : "no cheese"}},
+        5: {toppings : { pepperoni : "pepperoni", veggie : "vegetables", meat : "spicy beef", "ham and pineapple" : "ham and pineapple"}},
      }
      console.log("Welcome to Chloe's Pizzeria, please select what pizza you would like from the options below.")
+     for (let i=1; i<6; ) {
+         let breadOptions = menu[i]
+         console.log(`Please choose your base : ${breadOptions.bread.thin} or ${breadOptions.bread.thick}`)
+         let userSelectedBread = await userInput();
+         const base = breadOptions.bread[userSelectedBread]
+         const valid = checkOptions(userSelectedBread, breadOptions.bread)
+         console.log(valid)
+         if (valid) {
+             saveUserOption(base, usersPizza)
+             i++
+         } else {
+             console.log(`no valid option pick from ${JSON.stringify(breadOptions.bread)}`)
+             continue
+         }
 
-     let breadOptions = menu["1"]
-     console.log(`Please choose your base : ${breadOptions.bread.thin} or ${breadOptions.bread.thick}`)
-     let userSelectedBread = await userInput();
-     const base = breadOptions.bread[userSelectedBread]
-     saveUserOption(base, usersPizza)
 
-    let crustOptions = menu["2"]
-    console.log(`Please choose your crust : ${crustOptions.crust.garlic} or ${crustOptions.crust.stuffed}`)
-    let userSelectedCrust = await userInput();
-   const roll = crustOptions.crust[userSelectedCrust]
-    saveUserOption(roll, usersPizza)
+         let crustOptions = menu[i]
+         console.log(`Please choose your crust : ${crustOptions.crust.garlic} or ${crustOptions.crust.stuffed}`)
+         let userSelectedCrust = await userInput();
+         const roll = crustOptions.crust[userSelectedCrust]
+         const validCrust = checkOptions(userSelectedCrust, crustOptions.crust)
+         if (validCrust) {
+             saveUserOption(roll, usersPizza)
+             i++
+         } else {
+             console.log(`no valid option pick from ${JSON.stringify(breadOptions.crust)}`)
+             continue
+         }
 
-    let sauceOptions = menu["3"]
-    console.log(`Which sauce would you like on your pizza? : ${sauceOptions.sauce.tomato} or ${sauceOptions.sauce.bbq} or ${sauceOptions.sauce.none}`)
-    let userSelectedSauce = await userInput();
-     const sauce = sauceOptions.sauce[userSelectedSauce]
-    saveUserOption(sauce, usersPizza)
 
-    let cheeseOptions = menu["4"]
-    console.log(`Which cheese would you like on your pizza? : ${cheeseOptions.cheese.moz} ,
+         let sauceOptions = menu[i]
+         console.log(`Which sauce would you like on your pizza? : ${sauceOptions.sauce.tomato} or ${sauceOptions.sauce.bbq} or ${sauceOptions.sauce.none}`)
+         let userSelectedSauce = await userInput();
+         const sauce = sauceOptions.sauce[userSelectedSauce]
+         const validSauce = checkOptions(userSelectedCrust, sauceOptions.sauce)
+         if (validSauce) {
+             saveUserOption(sauce, usersPizza)
+             i++
+         } else {
+             console.log(`no valid option pick from ${JSON.stringify(sauceOptions.sauce)}`)
+             continue
+         }
+
+
+         let cheeseOptions = menu[i]
+         console.log(`Which cheese would you like on your pizza? : ${cheeseOptions.cheese.mozzarella} ,
      ${cheeseOptions.cheese.cheddar} , 
      ${cheeseOptions.cheese.vegan} ,
-      ${cheeseOptions.cheese.noCheese}`)
-    let userSelectedCheese = await userInput();
-     const cheeses = cheeseOptions.cheese[userSelectedCheese]
-    saveUserOption(cheeses, usersPizza)
+      ${cheeseOptions.cheese["no cheese"]}`)
+         let userSelectedCheese = await userInput();
+         const cheeses = cheeseOptions.cheese[userSelectedCheese]
+         const validCheese = checkOptions(userSelectedCheese, cheeseOptions.cheese)
+         if (validCheese) {
+             saveUserOption(cheeses, usersPizza)
+             i++
+         } else {
+             console.log(`no valid option pick from ${JSON.stringify(cheeseOptions.cheese)}`)
+             continue
+         }
 
-    let toppingOptions = menu["5"]
-    console.log(`Please choose your toppings : ${toppingOptions.toppings.pep} ,
+         let toppingOptions = menu[i]
+         console.log(`Please choose your toppings : ${toppingOptions.toppings.pep} ,
     ${toppingOptions.toppings.meat} ,
     ${toppingOptions.toppings.veggie} ,
-    ${toppingOptions.toppings.tropical}`)
-    let userSelectedToppings = await userInput();
-     const toppings = toppingOptions.toppings[userSelectedToppings]
-    saveUserOption(toppings, usersPizza)
+    ${toppingOptions.toppings["ham and pineapple"]}`)
+         let userSelectedToppings = await userInput();
+         const toppings = toppingOptions.toppings[userSelectedToppings]
+         const validToppings = checkOptions(toppings, toppingOptions.toppings)
+         if (validToppings) {
+             saveUserOption(toppings, usersPizza)
+             i++
+         } else {
+             console.log(`no valid option pick from ${JSON.stringify(toppingOptions.toppings)}`)
+         }
+     }
 
     console.log(usersPizza.length)
     console.log(`user selected pizza : ${usersPizza.join(" ")} pizza`)
